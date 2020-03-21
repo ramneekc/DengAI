098da31bc04ef5d7b29a3a96d71f60842e656b1d
ramneekc <ramneekchatha@gmail>
2020-03-21 02:11:51 -0700

    Added two stories for tableau

--- tableau.js
+++ tableau.js
@@ -1,6 +1,9 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/DengAi_Analysis/Sheet17?:display_count=y&:origin=viz_share_link";
-
+    url = "https://public.tableau.com/views/DengAi_Analysis/Story1?:display_count=y&:origin=viz_share_link";
     var viz = new tableau.Viz(containerDiv, url);
+
+    var containerDiv2 = document.getElementById("vizContainer-2"),
+    url2 = "https://public.tableau.com/views/DengAi_Analysis/Story2?:display_count=y&:origin=viz_share_link";
+    var viz = new tableau.Viz(containerDiv2, url2);
 }
